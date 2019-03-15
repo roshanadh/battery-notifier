c9e7e54d12dfbe6f5cbaab44f3069e01c7992035
Roshan Adhikari <nahsorad@gmail.com>
2019-03-15 11:22:31 +0545

    Fix unplug notification

--- script.py
+++ script.py
@@ -9,22 +9,25 @@ toaster = ToastNotifier()
 isNotifiedLow = False
 isNotifiedHigh = False
 
-while(True):
-    battery = sensors_battery()
-    isPlugged = battery.power_plugged
-    batteryPercent = battery.percent
+battery = sensors_battery()
+isPlugged = battery.power_plugged
+batteryPercent = battery.percent
 
+while(True):
     if batteryPercent <= 35 and isPlugged == False and isNotifiedLow == False:
         toaster.show_toast("Plug the charger", "Your battery percentage is getting low. \nPlease plug the charger!")
         isNotifiedLow = True
+    elif batteryPercent <= 35 and isPlugged == True and isNotifiedLow == True:
+        isNotifiedLow = False
 
     elif batteryPercent >= 85 and isPlugged == True and isNotifiedHigh == False:
         toaster.show_toast("Unplug the charger", "Battery is now sufficiently charged. \nYou may unplug the charger!")
         isNotifiedHigh = True
+    elif batteryPercent >= 85 and isPlugged == False and isNotifiedHigh == True:
+        isNotifiedHigh = False 
 
-    elif isPlugged == True:
-        isNotifiedLow = False
-    elif isPlugged == False:
-        isNotifiedHigh = False
+    battery = sensors_battery()
+    isPlugged = battery.power_plugged
+    batteryPercent = battery.percent
 
     time.sleep(30)
