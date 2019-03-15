5708bba04eeb3fb525634139d415b8ba2c15abc9
Roshan Adhikari <nahsorad@gmail.com>
2019-03-15 09:47:28 +0545

    Fix recurring notifications

--- script.py
+++ script.py
@@ -5,14 +5,23 @@ from psutil import sensors_battery
 
 toaster = ToastNotifier()
 
+isNotifiedLow = False
+isNotifiedHigh = False
+
 while(True):
     battery = sensors_battery()
     isPlugged = battery.power_plugged
     batteryPercent = battery.percent
 
-    if batteryPercent <= 35 and isPlugged == False:
+    if batteryPercent <= 35 and isPlugged == False and isNotifiedLow == False:
         toaster.show_toast("Plug the charger", "Your battery percentage is getting low. \nPlease plug the charger!")
+        isNotifiedLow = True
 
-    elif batteryPercent >= 85 and isPlugged == True:
+    elif batteryPercent >= 85 and isPlugged == True and isNotifiedHigh == False:
         toaster.show_toast("Unplug the charger", "Battery is now sufficiently charged. \nYou may unplug the charger!")
+        isNotifiedHigh = True
 
+    elif isPlugged == True:
+        isNotifiedLow = False
+    elif isPlugged == False:
+        isNotifiedHigh = False
